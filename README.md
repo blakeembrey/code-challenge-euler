e13cae039fc7b6be49b2fe1420ee909be6268dd8
Blake Embrey <hello@blakeembrey.com>
2014-09-08 12:02:42 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Code Challenge: Project Euler
+
+[![NPM version][npm-image]][npm-url]
+[![Gittip][gittip-image]][gittip-url]
+
+## Installation
+
+```sh
+npm install -g code-challenge-euler
+```
+
+## Usage
+
+```js
+challenge select        # "Project Euler"
+challenge exercise      # "Problem ..."
+challenge verify [file] # Does my solution work?
+```
+
+## Miscellaneous
+
+The exercise text, solutions and resources were pulled from [EulerPy](https://github.com/iKevinY/EulerPy). All resources were originally provided by [Project Euler](https://projecteuler.net/).
+
+## License
+
+MIT
+
+[npm-image]: https://img.shields.io/npm/v/code-challenge-euler.svg?style=flat
+[npm-url]: https://npmjs.org/package/code-challenge-euler
+[gittip-image]: https://img.shields.io/gittip/blakeembrey.svg?style=flat
+[gittip-url]: https://www.gittip.com/blakeembrey
---
+++ euler.js
@@ -0,0 +1,76 @@
+var fs        = require('fs');
+var join      = require('path').join;
+var assert    = require('assert');
+var challenge = require('code-challenge');
+var resources = require('./data/resources');
+
+/**
+ * Location of the problem file.
+ *
+ * @type {String}
+ */
+var PROBLEM_FILE = join(__dirname, 'data/problems.txt');
+
+/**
+ * Location of the solution file.
+ *
+ * @type {String}
+ */
+var SOLUTIONS_FILE = join(__dirname, 'data/solutions.txt');
+
+/**
+ * Set the challenge name.
+ *
+ * @type {String}
+ */
+challenge.name = 'Project Euler';
+
+/**
+ * Map of solutions.
+ *
+ * @type {Array}
+ */
+var solutions = fs.readFileSync(SOLUTIONS_FILE, 'utf8')
+  .split('\n')
+  .map(function (solution, index) {
+    return solution.replace((index + 1) + '. ', '');
+  });
+
+/**
+ * Read the problem file and split into sets.
+ */
+fs.readFileSync(PROBLEM_FILE, 'utf8')
+  .split('\n\n\n')
+  .forEach(function (problemText, index) {
+    var name = 'Problem ' + (index + 1);
+    var text = problemText.substr(name.length * 2 + 3);
+
+    challenge.exercise(name)
+      .add('print', function () {
+        var resource = resources[index + 1];
+
+        // Make sure the resource is an array.
+        if (resource && !Array.isArray(resource)) {
+          resource = [resource];
+        }
+
+        console.log(text);
+
+        if (resource) {
+          console.log();
+          console.log('Resources:');
+          console.log();
+
+          // Log each of the resources.
+          resource.forEach(function (file) {
+            console.log(join(__dirname, 'data/resources', file));
+          });
+        }
+      })
+      .add('verify', function () {
+        return challenge.execFile(this._[0])
+          .spread(function (stdout) {
+            assert.equal(stdout.replace(/\r?\n$/, ''), solutions[index]);
+          });
+      });
+  });
